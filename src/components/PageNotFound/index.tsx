2dc0579298ee2260693f5b4f4cffef692c7d4b01
vibueno <20514260+vibueno@users.noreply.github.com>
2021-06-18 00:54:52 +0200

    404 page changes
    - remove back button
    - Add better explanation

--- src/components/PageNotFound/index.tsx
+++ src/components/PageNotFound/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-import { useHistory } from 'react-router';
 import useBackground from '../../hooks/useBackground';
-
-import Button from '../../components/Button';
 
 import { msgPageNotFound1, msgPageNotFound2 } from '../../vars/messages';
 
@@ -12,22 +9,10 @@ import './index.css';
 const PageNotFound = () => {
   useBackground('bg-page-not-found');
 
-  const history = useHistory();
-
-  const onClickHandler = () => {
-    history.push('/');
-  };
-
   return (
     <div className="page-not-found-container">
       <div className="page-not-found-text">{msgPageNotFound1}</div>
       <div className="page-not-found-text">{msgPageNotFound2}</div>
-      <Button
-        id="btn-pagenotfound-return"
-        label="&#9003; Go home"
-        btnStyle="btn-back btn-pagenotfound"
-        clickHandler={onClickHandler}
-      />
     </div>
   );
 };
